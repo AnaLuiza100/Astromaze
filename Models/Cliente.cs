d591d8fdcb088622d84d7e9a107a118d33514ae2
220336 <220336@LTI5204>
2024-06-20 16:32:35 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Astromaze.Models
+{
+    public class Cliente
+    {
+        public string id { get; set; }
+        public string Especie { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string Tel { get; set; }
+        public string Idade { get; set; }
+    }
+}
